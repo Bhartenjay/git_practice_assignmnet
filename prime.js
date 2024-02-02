7cc13e4c1c55780bfe942f50af07033771084796
Bhartenjay Giri <bhartenjaygiri@gmail.com>
2024-02-02 23:16:40 +0530

    modified by Bhartenjay

--- prime.js
+++ prime.js
@@ -1,5 +1,12 @@
 let num =5;
-
+let count =0;
 for(let i=0; i<num; i++){
     
+    if(num%i==0){
+        count++ 
+    }
+}if(count==2){
+    console.log(num + ' is prime number')
+}else{
+    console.log(num + ' is not a prime number')
 }
