375e9bf75cda18e4e346b4a39d3d8c95e0c786b0
~Rahul <rborage97@gmail.com>
2024-02-02 23:02:43 +0530

    Prime correct by rahul

--- prime.js
+++ prime.js
@@ -1,5 +1,16 @@
 let num =5;
 
-for(let i=0; i<num; i++){
-    
+
+let fact=0;
+for(let i=1; i<=num; i++){
+    if(num%i==0){
+        fact++
+    }
+}
+
+if(fact==2){
+  console.log("Num is Prime");
+}
+else {
+    console.log("Num is Not Prime");
 }
